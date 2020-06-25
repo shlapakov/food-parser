06d0f3735aa3592a94aadcf5ac89eb1e3a7c58bc
fakeshluck <fakeshluck@gmail.com>
2020-06-25 18:40:49 +0300

    added Gemfile & get links

--- category.rb
+++ category.rb
@@ -1,6 +1,7 @@
 require 'curb'
 require 'nokogiri'
 
+$XPATH_PRODUCT = '//div[@class="product-container"]/div/div/a/@href'
 
 class Category
   def initialize(url)
@@ -8,9 +9,17 @@ class Category
     @links_to_products = []
   end
 
-  def get_list_of_products
+  def get_site_html
     http = Curl.get(@url)
-    html = Nokogiri::HTML(http.body)
-    puts html
+    @html = Nokogiri::HTML(http.body)
   end
+
+  def get_links_to_products
+    links = @html.xpath($XPATH_PRODUCT)
+    links.each do |link|
+      @links_to_products << link.value
+    end
+    puts @links_to_products
+  end
+
 end
